dc247a588d8c767dc0f362ee992493364eb432fd
farhanbaehaki <farhanm.1219@gmail.com>
2025-12-31 04:09:46 +0700

    barulagi1

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.unobserve(entry.target);
       }
     });
-  }, {threshold:0.1});
+  }, {threshold:0});
 
   galleryImgs.forEach((img,i)=>{
     img.style.setProperty('--i',i);
